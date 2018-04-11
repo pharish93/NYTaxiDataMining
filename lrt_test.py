df2f60c1ca48b6717325ee78d060596b5f53bc0b
kvtran2 <kvtran2@ncsu.edu>
2018-04-11 18:31:04 -0400

    Update lrt_test

--- lrt_test.py
+++ lrt_test.py
@@ -1,15 +1,19 @@
 from data_loading.data_loading import *
 from data_preprocessing.data_pre_process import *
-from mle import *
+from lrt.mle import *
+import matplotlib.pyplot as plt  # for plotting
 
 def main():
+
     train_df = load_train_data()
     train_df = data_preprocessing(train_df)
+    plt.savefig('a1.png')
+    lrt_cols = ['total_distance', 'total_travel_time', 'label_pick', 'label_drop', 'distance_haversine', 'trip_duration']
+    train_data = train_df[lrt_cols]
 
-    print(train_df.columns)
+    p_values = lrt_taxi_data(train_data)
 
-    #p_values = lrt_taxi_data(train_df)
-
+    print(p_values)
 
 if __name__ == '__main__':
     main()
